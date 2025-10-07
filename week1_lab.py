98cfcc7bae5cdc359f10ae76ce9ef051f7d490ff
charlie-pendleton <26460165@edgehill.ac.uk>
2025-10-07 14:47:22 +0100

    complete week1 lab challenges

---
+++ week1_lab.py
@@ -0,0 +1,10 @@
+print("hello world")
+student_name = "Charlie"
+student_age = 18
+print(f"Student Name: {student_name}")
+print(f"Student Age: {student_age}")
+
+def sum(a,b):
+    return a + b
+
+print(f"the sum is: {sum(10,15)}")
